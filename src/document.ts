5d7de6e6abcf7c7d0dec94de229ebe29586455b3
Sriram Kailasam <sriramkailasam99@gmail.com>
2019-06-26 20:46:29 +0530

    Initial commit

---
+++ src/document.ts
@@ -0,0 +1,9 @@
+export interface Document {
+    objectId: string;
+    filename: string;
+    project: string;
+    value: string;
+    author: string;
+    createdTs: Date;
+    modifiedTs: Date;
+}
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express = require("express");
+import http = require("http");
+import io = require("socket.io");
+import path = require("path");
+
+import { Document } from "./document";
+
+const app = express();
+const httpServer = new http.Server(app);
+
+const socketServer = io(httpServer);
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+socketServer.on("connection", socket => {
+    socket.on("join-project", (projectId: string) => {
+        socket.join(projectId);
+    });
+});
+
+socketServer.on("file-change", (document: Document) => {
+    const project = document.project;
+    socketServer.to(project).emit("file-change", document);
+});
+
+const port = process.env.PORT;
+httpServer.listen(port, () => {
+    console.log("Server started on port", port);
+});
